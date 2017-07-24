172602114b8bf887ad85a38ecd27180e84f5823c
raledong <raledong@gmail.com>
2017-07-24 13:56:36 +0800

    update 97 interleaving string

--- src/string/InterleavingString_97.java
+++ src/string/InterleavingString_97.java
@@ -63,4 +63,24 @@ public class InterleavingString_97 {
 		}
 		return matrix[s2.length()][s1.length()];
 	}
+	
+	public boolean isInterleave4(String s1, String s2, String s3){
+		if ((s1.length()+s2.length())!=s3.length()) return false;
+		boolean[] matrix = new boolean[s2.length()+1];
+		for(int i = 0 ; i<=s1.length() ; i++){
+			for(int j = 0 ; j<=s2.length() ; j++){
+				if(i==0&&j==0){
+					matrix[j] = true;
+				}else if(i==0){
+					matrix[j] = matrix[j-1] && s2.charAt(j-1)==s3.charAt(j-1);
+				}else if(j==0){
+					matrix[j] = matrix[j] && s1.charAt(i-1) == s3.charAt(i-1);
+				}else{
+					matrix[j] = (matrix[j-1] && s2.charAt(j-1)==s3.charAt(i+j-1))
+							|| (matrix[j] && s1.charAt(i-1)==s3.charAt(i+j-1));
+				}
+			}
+		}
+		return matrix[s2.length()];
+	}
 }
