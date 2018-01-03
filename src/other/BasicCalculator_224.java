b730e633203f71c4418c996ddd0bcb9072790651
raledong <raledong@gmail.com>
2018-01-03 16:35:03 +0800

    224 basic calculator

---
+++ src/other/BasicCalculator_224.java
@@ -0,0 +1,55 @@
+package other;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * @author rale
+ *
+ * Implement a basic calculator to evaluate a simple expression string.
+ * 
+ * The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+ * 
+ * You may assume that the given expression is always valid.
+ * 
+ * Some examples:
+ * "1 + 1" = 2
+ * " 2-1 + 2 " = 3
+ * "(1+(4+5+2)-3)+(6+8)" = 23
+ */
+public class BasicCalculator_224 {
+	
+	public  int calculate(String s) {
+		int len = s.length(), sign = 1, result = 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		for (int i = 0; i < len; i++) {
+			if (Character.isDigit(s.charAt(i))) {
+				int sum = s.charAt(i) - '0';
+				while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
+					sum = sum * 10 + s.charAt(i + 1) - '0';
+					i++;
+				}
+				result += sum * sign;
+			} else if (s.charAt(i) == '+')
+				sign = 1;
+			else if (s.charAt(i) == '-')
+				sign = -1;
+			else if (s.charAt(i) == '(') {
+				stack.push(result);
+				stack.push(sign);
+				result = 0;
+				sign = 1;
+			} else if (s.charAt(i) == ')') {
+				result = result * stack.pop() + stack.pop();
+			}
+
+		}
+		return result;
+	}
+	
+	public static void main(String[] args){
+		BasicCalculator_224 b = new BasicCalculator_224();
+		b.calculate("(1-(3-4))");
+	}
+
+}
