0580ffecb0e7aec01cb3eafdb806ae3eb2404268
raledong <raledong@gmail.com>
2018-02-12 16:17:45 +0800

    278 first bad version

---
+++ src/other/FirstBadVersion_278.java
@@ -0,0 +1,32 @@
+package other;
+
+/**
+ * @author rale
+ * You are a product manager and currently leading a team to develop a new product. 
+ * Unfortunately, the latest version of your product fails the quality check. 
+ * Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, 
+ * which causes all the following ones to be bad.
+ * 
+ * You are given an API bool isBadVersion(version) which will return whether version is bad. 
+ * Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+ */
+public class FirstBadVersion_278 {
+	
+	public int firstBadVersion(int n) {
+		int left = 1, right = n, mid = (left + right) / 2;
+		while(left < right-1){
+			if(isBadVersion(mid)){
+				right = mid;
+			}else{
+				left = mid;
+			}
+			mid = (left + right) / 2;
+		}
+		return right;
+    }
+	boolean isBadVersion(int version){
+		return version%2==0;
+	}
+}
