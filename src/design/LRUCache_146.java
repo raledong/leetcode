ac98b2d5826b0f233f13c4ef5021052229819379
raledong <raledong@gmail.com>
2018-03-01 20:30:58 +0800

    146 LRU CACHE 新增用LinkedHashMap的实现

--- src/design/LRUCache_146.java
+++ src/design/LRUCache_146.java
@@ -132,6 +132,26 @@ public void put(int key, int value) {
 	}
 	
 }
+
+//用LinkedHashMap实现
+public class LRUCache {
+    private LinkedHashMap<Integer, Integer> map;
+    private final int CAPACITY;
+    public LRUCache(int capacity) {
+        CAPACITY = capacity;
+        map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true){
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > CAPACITY;
+            }
+        };
+    }
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+    public void set(int key, int value) {
+        map.put(key, value);
+    }
+}
     public static void main(String[] args){
     	LRUCache_146 cache = new LRUCache_146(2);
     	cache.put(1, 1);
